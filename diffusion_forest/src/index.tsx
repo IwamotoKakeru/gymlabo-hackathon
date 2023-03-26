309488d4c4f3b68f6ccd2fdd41cfc5b3ae856aac
IwamotoKakeru <kganpipi@gmail.com>
2023-03-26 21:56:42 +0900

    Add gitignore

--- diffusion_forest/src/index.tsx
+++ diffusion_forest/src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {ChakraProvider} from "@chakra-ui/react"
+
+var titleName: string = "Diffusion Forest";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+document.title = titleName;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
