341283933d53916b00f3d129d53bc85c75ddcb2d
srpkdyy <srpkdyy@gmail.com>
2023-03-27 12:10:55 +0900

    Implement random prompt func

---
+++ diffusion_forest_server/prompt.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get(idx=None, lang='en'):
+    file = 'data/prompts_' + lang + '.txt'
+
+    with open(file) as f:
+        prompts = f.read().split('\n')
+
+    n = len(prompts)
+
+    if idx is None:
+        idx = random.randint(0, len(prompts)-1)
+
+    return prompts[idx]
+
