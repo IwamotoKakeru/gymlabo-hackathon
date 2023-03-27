c9f444e96fd8175c8d039d3f6c84adfdf8b4c4b3
pointn <code.club.nagahashi@gmail.com>
2023-03-27 15:00:54 +0900

    勝手に始まる90秒タイマーをGameの左上に配置

--- diffusion_forest/src/components/GameScene.tsx
+++ diffusion_forest/src/components/GameScene.tsx
@@ -5,13 +5,16 @@ import {
   Center,
   VStack,
   Heading,
+  Box,
 } from "@chakra-ui/react";
+import GameTimer from "./Timer";
 
 const GameScene = () => {
   const navigate = useNavigate();
 
   return (
     <ChakraProvider theme={theme}>
+      <Box pos="absolute" top="0" left="20"><GameTimer /></Box>
       <VStack>
         <Center h="520">
           <Heading fontSize={128}>Game</Heading>
