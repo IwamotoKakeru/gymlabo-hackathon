334c2395aba8a0ae3849cf53baafb8e52784a5a1
IwamotoKakeru <kganpipi@gmail.com>
2023-03-27 20:44:15 +0900

    Edit

--- diffusion_forest/src/components/GameScene.tsx
+++ diffusion_forest/src/components/GameScene.tsx
@@ -14,17 +14,22 @@ import useCountdown from "@bradgarropy/use-countdown";
 const GameScene = () => {
   const navigate = useNavigate();
   const countdown = useCountdown({
-    minutes:1,
-    seconds:0,
-    onCompleted: () => setTimeout(() => {navigate("/Result",{state: {score:100}})},3000)
+    minutes: 1,
+    seconds: 0,
+    onCompleted: () =>
+      setTimeout(() => {
+        navigate("/Result", { state: { score: 100 } });
+      }, 3000),
   });
   const timeLeft = countdown.minutes * 60 + countdown.seconds;
-  const padTime = String(timeLeft).padStart(2,'0');
+  const padTime = String(timeLeft).padStart(2, "0");
 
   return (
     <ChakraProvider theme={theme}>
       <Box pos="absolute" top="0" left="20">
-        <Heading fontSize={96}>{timeLeft===0 ? "finished!" : padTime}</Heading>
+        <Heading fontSize={96}>
+          {timeLeft === 0 ? "finished!" : padTime}
+        </Heading>
       </Box>
       <VStack>
         <Center h="520">
