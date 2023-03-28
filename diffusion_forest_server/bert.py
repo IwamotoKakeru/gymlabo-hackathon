7c61bdbc62e609f42723728e66ec83054f81a762
tebii <tebasaki.wejobs@gmail.com>
2023-03-28 13:20:17 +0900

    #24 sentence-transformersでの評価に変更

--- diffusion_forest_server/bert.py
+++ diffusion_forest_server/bert.py
@@ -1,7 +1,12 @@
+import torch
 import onnxruntime
 import numpy as np
 from transformers import AutoTokenizer
+from sentence_transformers import SentenceTransformer
 
+
+sentformer = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+cos_sim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
 
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 bert = onnxruntime.InferenceSession('onnx/bert.onnx')
@@ -12,6 +17,12 @@ def dict2numpy(d):
 
 
 def calc_similarity(two_sentences):
+    emb = sentformer.encode(two_sentences, convert_to_tensor=True)
+    sim = cos_sim(*emb)
+    return sim.item()
+
+
+def calc_similarity_legacy(two_sentences):
     tokens = tokenizer(two_sentences, padding=True)
     input_data = dict2numpy(tokens)
 
@@ -21,4 +32,3 @@ def calc_similarity(two_sentences):
     sim = np.dot(f1, f2) / (np.linalg.norm(f1) * np.linalg.norm(f2))
 
     return sim.item()
-
